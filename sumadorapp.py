cf7c1ce116a1d2aea5137fced0154e881ce783ef
Borja Egea Madrid <boegma@hotmail.com>
2016-03-01 12:35:52 +0100

    Ejercicio 14.8 sumador orientado a objetos

--- sumadorapp.py
+++ sumadorapp.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+# Borja Egea Madrid
 
 import socket
 
@@ -16,57 +17,60 @@ class webApp:
 		mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		mySocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 		mySocket.bind((hostname, port))
-	
+
 		# Queue a maximum of 5 TCP connection requests
 		mySocket.listen(5)
-	
+
 		# Accept connections, read incoming data, and call
 		# parse and process methods (in a loop)
-		
+
 		primer = None
 		while True:
-			print 'Waiting for connections'
+			print ('Waiting for connections')
 			(recvSocket, address) = mySocket.accept()
-			print 'HTTP request received (going to parse and process):'
+			print ('HTTP request received (going to parse and process):')
 			request = recvSocket.recv(2048)
-			print request
+			print (request)
 			parsedRequest = self.parse(request)
 			(returnCode, htmlAnswer) = self.process(parsedRequest)
-			print 'Answering back...'
-			recvSocket.send("HTTP/1.1 " + returnCode + " \r\n\r\n"
-					+ htmlAnswer + "\r\n")
-	
+			print ('Answering back...')
+			recvSocket.send(bytes("HTTP/1.1 " + returnCode + " \r\n\r\n"
+					+ htmlAnswer + "\r\n", 'utf-8'))
+			recvSocket.close()
+
 class sumadorApp(webApp):
-	
+
 	def parse(self, request):
-		return numero
-	
+		try:
+			numero  = int(request.split()[1][1:])
+			valido = True
+		except ValueError:
+			valido = False
+			numero = 0
+		return numero, valido
+
 	def process(self, parsedRequest):
-			suma = None		
-			primer = None
-
-	def process(self, parsedRequest, primer):
-
-			if primer == None:
-				primer = parsedRequest
+			numero, valido = parsedRequest
+			if not valido:
+				return ("200 OK", "<html><body><h1>Solo manejo enteros </h1></body></html>")
+			if self.esPrimero:
+				self.primer_num = numero
+				self.esPrimero = False
 				return ("200 OK", "<html><body><h1>Dame otro numero </h1></body></html>")
-	
+
 			else:
-				num = parsedRequest
-				try:
-					suma = int(primer) + int(num)	
-					return("HTTP/1.1 200 OK\r\n\r\n" +
-							"<html><body><h1>" + str(primer) + " + " + str(num) + " = " + str(suma) 
-							+ "</h1></body></html>" + "\r\n")
-	
-					primer = None			
+				segundo_num = numero
+				suma = self.primer_num + segundo_num
+				self.esPrimero = True
+				return("HTTP/1.1 200 OK\r\n\r\n",
+						"<html><body><h1>" + str(self.primer_num) + " + " + str(segundo_num) + " = " + str(suma)
+						+ "</h1></body></html>" + "\r\n")
 
-				except ValueError:
-					return("HTTP/1.1 400 Error..\r\n\r\n" +
-							"<html><body><h1>No podemos sumar enteros y caracteres</h1></body></html>"
-							+"\r\n")
 
-					primer = None
+
+	def __init__(self,hostname, port):
+			self.esPrimero = True
+			super(sumadorApp, self).__init__(hostname,port)
 
 if __name__ == "__main__":
 	sumador = sumadorApp("localhost", 8080)
